1aa057e6d3ef0bb8ca662489c1c969dbce32d02a
Nyakwara Festus <festusnyakwara@Gmail.com>
2026-05-21 11:05:28 +0300

    configured routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Home from './Components/Home.jsx'
+import About from './Components/About.jsx'
 
 const App = () => {
   return (
     <div>
-      
+      <Routes >
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+      </Routes> 
     </div>
   )
 }
